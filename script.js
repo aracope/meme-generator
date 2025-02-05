38e0e5cb3c2bf558110b37f2159d23cceee64496
aracope <156398622+aracope@users.noreply.github.com>
2025-02-05 03:31:38 -0800

    Refactor functions to create meme elements.

--- script.js
+++ script.js
@@ -1,8 +1,56 @@
 document.addEventListener("DOMContentLoaded", function () {
   const memeForm = document.querySelector('.meme-input-form');
-  const memeGallery = document.querySelector('.meme-gallery');
+  const memeMuseum = document.querySelector('.meme-museum');
 
+  // Helper function to create meme element with text
+  function createMemeElement(tag, className, content = '') {
+    const element = document.createElement(tag);
+    if (className) element.classList.add(className);
+    element.innerHTML = content;
+    return element;
+  }
+
+  // Create the meme element with image and text
+  function createMeme(imageUrl, topText, bottomText) {
+    if (!imageUrl) {
+      alert("Please provide a valid image URL!");
+      return;
+    }
+    // Create meme-gallery div and image
+    const memeGallery = createMemeElement('div', 'meme-gallery')
+    const memeImage = createMemeElement('img', 'meme-image')
+    memeImage.src = imageUrl;
+    memeImage.alt = "Image from URL input.";
+
+    // Create top and bottom text divs
+    const topText = createMemeElement('div', 'top-text', topText);
+    const bottomText = createMemeElement('div', 'bottom-text', bottomText);
+
+    // Create 'x' to delete a meme
+    const deleteSpan = createMemeElement('span', 'delete-meme', '\u00d7');
+
+    // Append elements to the memeGallery
+    memeGallery.append(memeImage, topText, bottomText, deleteSpan);
+
+    // Append to memeMuseum and save
+    memeMuseum.appendChild(memeGallery);
+    saveMeme();
+  }
+
+  // Meme form submission
+  function memeFormSubmit(event) {
+    event.preventDefault();
+
+    const imageUrl = document.getElementById('img-url').value;
+    const topText = document.getElementById('top-text-input').value;
+    const bottomText = document.getElementById('bottom-text-input').value;
+
+    createMeme(imageUrl, topText,bottomText);
+    memeForm.reset();
+  }
   
+  memeForm.addEventListener("submit", memeFormSubmit);
+  deleteSpan.addEventListener("click", memeDeletion);
 });
 
 
